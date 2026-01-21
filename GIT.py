3713181128e04082ff07c40321b7f9a6705c6fa1
muhammadfajrulsutagalang-dotcom <muhammadfajrulsutagalang@smkitihsanulfikri.sch.id>
2026-01-21 11:22:34 +0700

    Update GIT.py

--- GIT.py
+++ GIT.py
@@ -2,4 +2,5 @@ Belajar Git Source Control
 
 Nama lengkap kam: SUTA
 
-Saya senang belajar Git source control di Gamelan Indonesia
+
+Saya senang belajar Git source control di Gamelan Indonesia kerena sangat menyenangkan Bisa belajar Hal baru di sini
